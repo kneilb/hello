266e3cb1d00e36520dfda2d586c8b28b32168871
Neil Burningham <neil.burningham@mediakind.com>
2023-11-29 21:46:57 +0000

    Single threaded from-scratch web server

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::time::Duration;
+use std::{fs, thread};
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    // TODO: proper buffer handling + read_all ??
+    stream.read(&mut buffer).unwrap();
+
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let root_get = b"GET / HTTP/1.1\r\n";
+    let sleep_get = b"GET /sleep HTTP/1.1\r\n";
+    // let favicon_get = b"GET /favicon.ico HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(root_get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep_get) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        content.len(),
+        content
+    );
+    // TODO: proper buffer handling + write_all ??
+    stream.write(response.as_bytes()).unwrap();
+}
